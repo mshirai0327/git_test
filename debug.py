de661bbe2975b95a94674eb682aedf1b57e7d37a
mshirai0327 <nnshiro@gmail.com>
2018-10-09 04:50:31 +0900

    hello git

---
+++ debug.py
@@ -0,0 +1,31 @@
+from texasholdem_Dealer import Card,Dealer
+import random
+from texasholdem_Player import Player
+
+#カードタプルを生成して関数に渡して動作確認するためのプログラム#
+
+player1 = Player()
+player2 = Player()
+player3 = Player()
+player4 = Player()
+
+players_list = [player1, player2, player3, player4]
+random.shuffle(players_list)
+dealer=Dealer(players_list)
+
+for k in range(10000):
+    print("")
+    print("game=",k)
+    cards=[0]*7
+    suit=['S','C','H','D']
+
+    for i in range(7):#7cards
+        k=random.randrange(4)
+        number=random.randrange(13)+1
+        card=Card(suit[k],number)
+        cards[i]=card
+
+    (sc,rtcards)=dealer.calc_hand_score(cards)
+    print("score--",sc)
+    print("best--",rtcards)
+
